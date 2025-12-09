26ddd947bb462c62660f42a632453274a4f162df
akhiljozph <ajpulluvelil@gmail.com>
2025-12-09 16:01:52 +0530

    PROM-6 - Added new logo in home page.

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,8 +1,10 @@
 import './App.css';
+import proManLogo from './assets/proman.png'
 
 function App() {
   return (
     <section className="app">
+      <img src={proManLogo} />
       <h1>ProMan</h1>
       <p>Project Management System</p>
     </section>
