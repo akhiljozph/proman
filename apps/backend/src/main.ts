a3742624ffa959008aec89161c89b990f5c49f74
akhiljozph <ajpulluvelil@gmail.com>
2025-12-10 23:23:25 +0530

    PROM-7 - Added environment variable support.

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,10 +1,16 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    const configService = app.get(ConfigService);
+
+    const port = configService.get<number>('PORT') || 3001;
+
     app.enableCors();
-    await app.listen(3001);
-    console.log('API running on http://localhost:3001');
+    await app.listen(port);
+    console.log(`API running on http://localhost:${port}`);
 }
 bootstrap();
